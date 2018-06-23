786835fccfe684321be5cabd767e3309bc376f6d
oscar20 <oscar_almazan@outlook.com>
2018-06-22 20:46:11 -0500

    Se agrego tap bar y ya funciona

--- 06_VistasProgramaticas/ConversionViewController.swift
+++ 06_VistasProgramaticas/ConversionViewController.swift
@@ -65,6 +65,7 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ConversionViewController loaded its view.")
         updateCelsiusLabel()
     }
     
---
+++ 06_VistasProgramaticas/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  06_VistasProgramaticas
+//
+//  Created by d182_oscar_a on 22/06/18.
+//  Copyright © 2018 d182_oscar_a. All rights reserved.
+//
+
+import UIKit
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view.")
+    }
+}
