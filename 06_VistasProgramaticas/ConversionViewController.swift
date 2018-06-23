f291f18a88e876c427b4085be0ab3b99678232e9
oscar20 <oscar_almazan@outlook.com>
2018-06-22 19:16:54 -0500

    Se hace la conversión pero el resultado muestra muchos dígitos

--- 06_VistasProgramaticas/ConversionViewController.swift
+++ 06_VistasProgramaticas/ConversionViewController.swift
@@ -13,6 +13,11 @@ class ConversionViewController: UIViewController {
     
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
     
     //Quitar teclado
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
@@ -22,11 +27,37 @@ class ConversionViewController: UIViewController {
     //Funcion para colocar el texto en celsius label
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
         
-        if let text = textField.text, !text.isEmpty {
+        /*if let text = textField.text, !text.isEmpty {
             celsiusLabel.text = text
         } else {
             celsiusLabel.text = "???"
+        }*/
+        
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
         }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil }
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = "\(celsiusValue.value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
     }
     
     
