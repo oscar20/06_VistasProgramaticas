cde2cf1fc90d181234cf0c9e5f931b729321921e
oscar20 <oscar_almazan@outlook.com>
2018-06-22 19:07:19 -0500

    Se agrego dismiss en keyboard

--- 06_VistasProgramaticas/ConversionViewController.swift
+++ 06_VistasProgramaticas/ConversionViewController.swift
@@ -12,7 +12,22 @@ import UIKit
 class ConversionViewController: UIViewController {
     
     @IBOutlet var celsiusLabel: UILabel!
-    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-        celsiusLabel.text = textField.text
+    @IBOutlet var textField: UITextField!
+    
+    //Quitar teclado
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
     }
+    
+    //Funcion para colocar el texto en celsius label
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    
 }
